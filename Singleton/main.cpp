5d0501f4d6b19c668d8606bc15d1fa0bf601cb3b
Marcin <73531965+MarBor2000@users.noreply.github.com>
2022-05-09 23:30:01 +0200

    Update main.cpp

--- Singleton/main.cpp
+++ Singleton/main.cpp
@@ -45,8 +45,57 @@ void PlanTreningowy::pokazPlan() {
 
 int main() {
     PlanTreningowy::getInstance().dodajCw();
-    PlanTreningowy::getInstance().pokazPlan();
-    PlanTreningowy::getInstance().usunCw();
-    PlanTreningowy::getInstance().pokazPlan();
+    #include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+class PlanTreningowy {
+public:
+    void dodajCw();
+    void usunCw();
+    void pokazPlan();
+    static PlanTreningowy& getInstance() {
+        static PlanTreningowy instance;
+        return instance;
+    }
+private:
+    string plan;
+    PlanTreningowy(){};
+};
+
+void PlanTreningowy::dodajCw() {
+    string cwiczenie;
+    while(cwiczenie!="0") {
+        cin >> cwiczenie;
+        if(cwiczenie!="0") {
+            plan += cwiczenie;
+            plan += " ";
+        }
+    }
+}
+
+void PlanTreningowy::usunCw() {
+    int nr,nr2;
+    cin>>nr;
+    cin>>nr2;
+    plan.erase(nr,nr2);
+}
+
+
+void PlanTreningowy::pokazPlan() {
+    cout<<plan<<"\n";
+}
+
+
+int main() {
+    PlanTreningowy p(PlanTreningowy::getInstance());
+    p.dodajCw();
+    p.pokazPlan();
+    p.usunCw();
+    p.pokazPlan();
+    
     return 0;
 }
