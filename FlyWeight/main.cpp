519c96e0513e9f1ed2f5b9e8cee8a13c0215c842
Marcin <73531965+MarBor2000@users.noreply.github.com>
2022-06-15 13:17:55 +0200

    Add files via upload

---
+++ FlyWeight/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+class Texture {
+public:
+	int id;
+	string color;
+	Texture(int id, string color);
+};
+
+Texture::Texture(int id, string color) {
+    this->id=id;
+    this->color=color;
+}
+
+
+class Mob {
+public:
+	Mob(string name, Texture* texture);
+private:
+    string name;
+	Texture* texture;
+};
+
+Mob::Mob(string name, Texture* texture) {
+    this->name=name;
+    this->texture=texture;
+}
+
+
+class TextureFactory {
+private:
+    vector<Texture*>* textures;
+public:
+	TextureFactory();
+	Texture* createTex(int id, string color);
+};
+
+TextureFactory::TextureFactory() {
+    this->textures = new vector<Texture*>();
+}
+
+Texture* TextureFactory::createTex(int id, string color) {
+    this->textures->push_back(new Texture(id,color));
+    return this->textures->back();
+}
+
+
+class Game {
+public:
+    Game(TextureFactory* textureFactory);
+	void setMob(string name,Texture* texture);
+private:
+    TextureFactory* textureFactory;
+	vector<Mob*>* mobs;
+};
+
+
+void Game::setMob(string name,Texture* texture) {
+    mobs->push_back(new Mob(name,texture));
+    cout<<"Create new mob "<< name<< " with id "<<texture->id<<" and color "<<texture->color<<endl;
+}
+
+Game::Game(TextureFactory* textureFactory) {
+    this->textureFactory=textureFactory;
+    this->mobs = new vector<Mob*>();
+}
+
+
+
+int main()
+{
+    TextureFactory* fac = new TextureFactory();
+    Game *game = new Game(fac);
+
+    Texture* tx = fac->createTex(01,"000000");
+    Texture* tx1 = fac->createTex(02,"ffffff");
+    game->setMob("Ork",tx1);
+    game->setMob("Wampir",tx);
+    return 0;
+}
